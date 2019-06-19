e40ad10503ddad2aa53aaad98971e1a6c6c2e40e
marko_rochevski <marko.rochevski@doxteam.com>
2019-06-19 09:15:02 +0200

    Removed more advanced stuff

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-1. Create and initialize npm project: npm init
+1. Create and initialize npm project
 2. Install typescript and express modules
 3. Setup typescript
 4. Write build, prebuild and start scripts
+5. Setup basic route and start server
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import * as bodyParser from "body-parser";
 
 const app = express();
-import { booksRouter } from "./routes/books";
 
-app.use("/books", booksRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/book", (req, res) => {
+    console.log("Success!");
+    res.send({ book: "Lord of the rings" });
+});
 
 app.listen(5000, () => {
     console.log("Server running on port 5000!");
